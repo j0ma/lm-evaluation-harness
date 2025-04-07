96f5ea384fa3d038a8de6497d8515b8893cf1731
Jonne Sälevä <jonnesaleva@brandeis.edu>
2025-04-07 12:20:41 -0400

    Move the scripts to scripts/

--- scripts/compute_comet_bootstraps.sh
+++ scripts/compute_comet_bootstraps.sh
@@ -6,7 +6,7 @@ export bootstrap_iters=${2:-100}
 export lang_pair=$(basename $jsonl_file | cut -f2,3 -d-)
 export jsonl_output_file=$(dirname $jsonl_file)/bootstrapped-comet-${lang_pair}.jsonl
 
-python bootstrap_comet.py \
+python scripts/bootstrap_comet.py \
     -b 100 \
     ${jsonl_file} \
     2> /dev/null \
