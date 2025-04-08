b01745445e116f68379765075b197ff37ae61f6b
Jonne Sälevä <jonne@jonnesaleva.com>
2025-04-08 15:21:39 -0400

    WIP: comment out functions:
    - process_docs
    - process_results
    - aggregation and
    - higher_is_better

--- lm_eval/tasks/flores/task.py
+++ lm_eval/tasks/flores/task.py
@@ -124,54 +124,54 @@ class FLORESTask(ConfigurableTask):
             )
         ]
 
-    def process_docs(self, doc):
-        """Take a single document and the LM results and evaluates, returning a
-        dict where keys are the names of submetrics and values are the values of
-        the metric for that one document
-
-        :param doc:
-            The document as returned from training_docs, validation_docs, or test_docs.
-        :param results:
-            The results of the requests created in construct_requests.
-        """
-        return doc
-
-    def process_results(self, doc, results):
-        """Take a single document and the LM results and evaluates, returning a
-        dict where keys are the names of submetrics and values are the values of
-        the metric for that one document
-
-        :param doc:
-            The document as returned from training_docs, validation_docs, or test_docs.
-        :param results:
-            The results of the requests created in construct_requests.
-        """
-
-        continuation = results[0]
-
-        predictions = continuation
-
-        references = doc
-        return {
-            metric.replace("metrics.", ""): (predictions, references)
-            for metric in self.metrics
-        }
-
-    def aggregation(self):
-        """
-        :returns: {str: [float] -> float}
-            A dictionary where keys are the names of submetrics and values are
-            functions that aggregate a list of metrics
-        """
-        return {
-            metric.replace("metrics.", ""): partial(score, metric=metric)
-            for metric in self.metrics
-        }
-
-    def higher_is_better(self):
-        """
-        :returns: {str: bool}
-            A dictionary where keys are the names of submetrics and values are
-            whether a higher value of the submetric is better
-        """
-        return {metric.replace("metrics.", ""): True for metric in self.metrics}
+    # def process_docs(self, doc):
+    #     """Take a single document and the LM results and evaluates, returning a
+    #     dict where keys are the names of submetrics and values are the values of
+    #     the metric for that one document
+    #
+    #     :param doc:
+    #         The document as returned from training_docs, validation_docs, or test_docs.
+    #     :param results:
+    #         The results of the requests created in construct_requests.
+    #     """
+    #     return doc
+    #
+    # def process_results(self, doc, results):
+    #     """Take a single document and the LM results and evaluates, returning a
+    #     dict where keys are the names of submetrics and values are the values of
+    #     the metric for that one document
+    #
+    #     :param doc:
+    #         The document as returned from training_docs, validation_docs, or test_docs.
+    #     :param results:
+    #         The results of the requests created in construct_requests.
+    #     """
+    #
+    #     continuation = results[0]
+    #
+    #     predictions = continuation
+    #
+    #     references = doc
+    #     return {
+    #         metric.replace("metrics.", ""): (predictions, references)
+    #         for metric in self.metrics
+    #     }
+    #
+    # def aggregation(self):
+    #     """
+    #     :returns: {str: [float] -> float}
+    #         A dictionary where keys are the names of submetrics and values are
+    #         functions that aggregate a list of metrics
+    #     """
+    #     return {
+    #         metric.replace("metrics.", ""): partial(score, metric=metric)
+    #         for metric in self.metrics
+    #     }
+    #
+    # def higher_is_better(self):
+    #     """
+    #     :returns: {str: bool}
+    #         A dictionary where keys are the names of submetrics and values are
+    #         whether a higher value of the submetric is better
+    #     """
+    #     return {metric.replace("metrics.", ""): True for metric in self.metrics}
